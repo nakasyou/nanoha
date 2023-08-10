b281621d51ebf2ad0bfd6b1ad06b5f464d17c4e9
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-10 12:35:40 +0900

    Change Notes

--- src/app/note/index.tsx
+++ src/app/note/index.tsx
@@ -1,7 +1,40 @@
+// import TextNote from "./components/TextNote.tsx"
+import {
+  IconPlayerPlay,
+  IconEdit,
+  IconEye,
+} from "@tabler/icons-react"
+import { useState } from "react"
+import classnames from "classnames"
+
 export interface Props {
+  
 }
 export default function(props: Props){
+  const [mode, setMode] = useState<"edit" | "play">("edit")
+  const [isView, setIsView] = useState(false)
+
   return <>
-    <div>NanohaNote</div>
+    <div>This is app!</div>
+    <TextNote mode={mode} isView={isView} />
+    
+    <div class="fixed bottom-0">
+      {/* Navbar */}
+      <div class="flex gap-4">
+        <div class="flex justify-center items-center bg-stone-100 rounded-full">
+          <button onClick={()=>setMode("edit")} className={classnames("p-4 rounded-full", { "bg-lime-300": mode === "edit" })}>
+            <IconEdit />
+          </button>
+          <button onClick={()=>setMode("play")} className={classnames("p-4 rounded-full", { "bg-lime-300": mode === "play" })}>
+            <IconPlayerPlay />
+          </button>
+        </div>
+        <div class="flex justify-center items-center gap-2">
+          <button onClick={()=>setIsView(!isView)} class="p-4 rounded-full drop-shadow-md bg-emerald-100 hover:bg-emerald-200">
+            <IconEye />
+          </button>
+        </div>
+      </div>
+    </div>
   </>
 }
