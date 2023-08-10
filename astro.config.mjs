ec768d4ae33d6baf26749e41daa31aeb521e21ee
nakasyou <syoutarou1103@outlook.jp>
2023-08-10 02:33:43 +0000

    Init react and TailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import deno from "@astrojs/deno";
+
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: deno()
+  adapter: deno(),
+  integrations: [react(), tailwind()]
 });
