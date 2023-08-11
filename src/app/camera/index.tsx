14c77fc1e522318ae08781140250065be5ef1f38
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-11 13:23:03 +0900

    Update index.tsx

--- src/app/camera/index.tsx
+++ src/app/camera/index.tsx
@@ -1,21 +1,27 @@
 import { useRef, useEffect } from "react"
 
 export default function () {
-  const videoRef = useRef<HTMLVideoElement | null>(null)
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
   useEffect(() => {
     (async () => {
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
         // カメラ使えない
         alert('お使いのデバイスには、ウェブカメラ機能が搭載されていません...')
       }
       const stream = await navigator.mediaDevices.getUserMedia({ video: true }).catch(() => alert('カメラへのアクセスを許可してください'))
       videoRef.current.srcObject = stream
       videoRef.current?.play()
+      const ctx = canvasRef.current.getContext('2d')
+      setInterval(() => {
+        ctx.drawImage(videoRef.current, 1000, 1000)
+      }, 10)
     })()
   }, [])
 
   return <>
     <video width="1000" height="1000" autoplay ref={videoRef} />
+    <canvas width="1000" height="1000" ref={canvasRef} />
     Hello camera!
   </>
 }
