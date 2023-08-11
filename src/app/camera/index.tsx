5a3adbb70a9ba5dd4c997e756f8b232810a4ae01
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-11 15:11:19 +0900

    Update index.tsx

--- src/app/camera/index.tsx
+++ src/app/camera/index.tsx
@@ -2,15 +2,13 @@ import { useRef, useEffect, useState } from "react"
 import { Camera, useCamera } from './utils/Camera.tsx'
 
 export default function () {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-  const [cameraStream, setCameraStream] = useState(null)
-
   const camera = useCamera({
     width: 1000,
     height: 100,
   })
   useEffect(() => {
     (async () => {
+      alert("x")
       const inputs = await camera.getInputs()
       await camera.startCamera(inputs[0])
     })()
