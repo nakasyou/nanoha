c96cc3a8fa68d4b5b021c9623185b29f4c9506fd
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-11 15:38:37 +0900

    Update index.tsx

--- src/app/camera/index.tsx
+++ src/app/camera/index.tsx
@@ -2,13 +2,14 @@ import { useRef, useEffect, useState } from "react"
 import { Camera, useCamera } from './utils/Camera.tsx'
 
 export default function () {
-  const camera = useCamera({
-    width: 1000,
-    height: 100,
-  })
   useEffect(() => {
     (async () => {
-      alert("x")
+      alert(useCamera)
+      const camera = useCamera({
+        width: 1000,
+        height: 100,
+      })
+      alert(camera)
       const inputs = await camera.getInputs()
       await camera.startCamera(inputs[0])
     })()
