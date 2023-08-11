c86d27dad79d9841e063855773d26468539c2ac5
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-11 13:16:22 +0900

    Update index.tsx

--- src/app/camera/index.tsx
+++ src/app/camera/index.tsx
@@ -1,8 +1,21 @@
 import { useRef } from "react"
 
 export default function () {
-  const videoRef = useRef<HTMLVideoElement>(null)
+  const videoRef = useRef<HTMLVideoElement | null>(null)
+  useEffect(() => {
+    (async () => {
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        // カメラ使えない
+        alert('お使いのデバイスには、ウェブカメラ機能が搭載されていません...')
+      }
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true }).catch(() => alert('カメラへのアクセスを許可してください'))
+      videoRef.current.srcObject = stream
+      videoRef.current?.play()
+    })()
+  }, [])
+
   return <>
-   Hello camera!
+    <video width="1000" height="1000" autoplay ref={videoRef} />
+    Hello camera!
   </>
 }
