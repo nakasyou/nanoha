83d9285b15135449b3dc81fc9ebca49c7c1be8ad
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-12 11:12:38 +0900

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,34 @@
+const { argbFromHex, themeFromSourceColor, applyTheme, hexFromArgb } = require("@material/material-color-utilities")
+const tailwindPlugin = require("tailwindcss/plugin")
+
+const materialColorPlugin = (baseColor) => {
+  const theme = themeFromSourceColor(argbFromHex(baseColor))
+  const props = Object.keys(theme.schemes.light.props)
+  const classes = props.map(prop => prop.replace(/[A-Z]/g, l => '-' + l.toLowerCase()))
+  const lightComponents = Object.assign(
+    Object.fromEntries(classes.map((query, index) => ['.bg-' + query, { 'background-color': hexFromArgb(theme.schemes.light[props[index]]) }])),
+    Object.fromEntries(classes.map((query, index) => ['.text-' + query, { 'color': hexFromArgb(theme.schemes.light[props[index]]) }]))
+  )
+  const darkComponents = Object.assign(
+    Object.fromEntries(classes.map((query, index) => ['.bg-' + query, { 'background-color': hexFromArgb(theme.schemes.dark[props[index]]) }])),
+    Object.fromEntries(classes.map((query, index) => ['.text-' + query, { 'color': hexFromArgb(theme.schemes.dark[props[index]]) }]))
+  )
+  const components = {
+    '@media (prefers-color-scheme: dark)': darkComponents,
+    '@media (prefers-color-scheme: light)': lightComponents,
+  }
+  return tailwindPlugin(({ addComponents }) => {
+    addComponents(components)
+  })
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+    materialColorPlugin('#f0e22f'),
+  ],
 }
