57a64e7fad96ce014468690cc0a0af46a794ab24
nakasyou <syoutarou110@outlook.jp>
2023-08-12 16:38:23 +0900

    Fix

--- src/app/note/index.tsx
+++ src/app/note/index.tsx
@@ -3,7 +3,8 @@ import {
   IconPlayerPlay,
   IconEdit,
   IconEye,
-  IconPlus
+  IconPlus,
+  IconScan
 } from "@tabler/icons-react"
 import { useState } from "react"
 import classnames from "classnames"
@@ -15,44 +16,59 @@ export default function(props: Props){
   const [mode, setMode] = useState<"edit" | "play">("edit")
   const [isView, setIsView] = useState(false)
 
+  const [plusFubActive, setPlusFubActive] = useState(false)
   return <>
-    <div>This is app!</div>
-    <div>
-      <TextNote mode={mode} isView={isView} defaultContent={`
-       <p>こんにちは！これはNanohaNoteです！</p>
-       <p>NanohaNoteは、「じぶん」で作る、学習用ノートブックです！</p>
-       <p>暗記をスムーズに行えます。</p>
-       <p>例えば、こんなことができちゃいます:</p>
-       <p>「Scratchでプログラミングするように、視覚的にプログラミングすることを、<span data-nanohasheet="true">ビジュアルプログラミング</span>という」</p>
-       <p>じゃーん。すごいでしょ。<b>こんなふうに太字</b>にしたり、<del>証拠隠滅</del>したりできます。</p>
-       <p>さあ、あなたの思いのままのノートにしましょう！この説明を消してもいいですよ〜</p>
-      `} />
-    </div>
-    <div className="fixed bottom-0 w-full bg-lime-200">
-      {/* Navbar */}
-      <div className="flex gap-4 justify-center items-center m-2">
-        <div className="flex justify-center items-center bg-lime-50 rounded-full">
-          <button onClick={()=>setMode("edit")} className={classnames("p-4 rounded-full", { "bg-lime-300": mode === "edit" })}>
-            <IconEdit />
-          </button>
-          <button onClick={()=>setMode("play")} className={classnames("p-4 rounded-full", { "bg-lime-300": mode === "play" })}>
-            <IconPlayerPlay />
-          </button>
+    <div className="bg-background text-on-background min-h-screen">
+      <div>This is app!</div>
+      <div>
+        <TextNote mode={mode} isView={isView} defaultContent={`
+        <p>こんにちは！これはNanohaNoteです！</p>
+        <p>NanohaNoteは、「じぶん」で作る、学習用ノートブックです！</p>
+        <p>暗記をスムーズに行えます。</p>
+        <p>例えば、こんなことができちゃいます:</p>
+        <p>「Scratchでプログラミングするように、視覚的にプログラミングすることを、<span data-nanohasheet="true">ビジュアルプログラミング</span>という」</p>
+        <p>じゃーん。すごいでしょ。<b>こんなふうに太字</b>にしたり、<del>証拠隠滅</del>したりできます。</p>
+        <p>さあ、あなたの思いのままのノートにしましょう！この説明を消してもいいですよ〜</p>
+        `} />
+      </div>
+      <div className="fixed bottom-0 w-full bg-secondary-container">
+        {/* Navbar */}
+        <div className="flex gap-4 justify-center items-center m-2">
+          <div className="flex justify-center items-center bg-lime-50 rounded-full">
+            <button onClick={()=>setMode("edit")} className={classnames("p-4 rounded-full", { "bg-lime-300": mode === "edit" })}>
+              <IconEdit />
+            </button>
+            <button onClick={()=>setMode("play")} className={classnames("p-4 rounded-full", { "bg-lime-300": mode === "play" })}>
+              <IconPlayerPlay />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="fixed bottom-10 right-4">
-      {/* 重要ボタンとか言うやつ */}
-      { mode === "play" && <div className="flex justify-center items-center gap-2">
-        <button className="p-4 rounded-2xl drop-shadow bg-white border hover:bg-lime-50" onClick={()=>setIsView(!isView)}>
-          <IconEye />
-        </button>
-      </div> }
-      { mode === "edit" && <div className="flex justify-center items-center gap-2">
-        <button className="p-4 rounded-2xl drop-shadow bg-white border hover:bg-lime-50">
-          <IconPlus />
-        </button>
-      </div> }
+      <div className="fixed bottom-10 right-4">
+        {/* 重要ボタンとか言うやつ */}
+        { mode === "play" && <div className="flex justify-center items-center gap-2">
+          <button className="fab" onClick={()=>setIsView(!isView)}>
+            <IconEye />
+          </button>
+        </div> }
+
+        { mode === "edit" && <>
+          <div className="flex justify-center items-center gap-2 flex-col">
+            {
+              plusFubActive && <div>
+                <button className="fab">
+                  <IconScan />
+                </button>
+              </div>
+            }
+            <button className="fab" onClick={() => {
+              setPlusFubActive(!plusFubActive)
+            }}>
+              <IconPlus />
+            </button>
+          </div>
+        </> }
+      </div>
     </div>
   </>
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,4 @@
-const { argbFromHex, themeFromSourceColor, applyTheme, hexFromArgb } = require("@material/material-color-utilities")
-const tailwindPlugin = require("tailwindcss/plugin")
-
-const materialColorPlugin = (baseColor) => {
-  const theme = themeFromSourceColor(argbFromHex(baseColor))
-  const props = Object.keys(theme.schemes.light.props)
-  const classes = props.map(prop => prop.replace(/[A-Z]/g, l => '-' + l.toLowerCase()))
-  const lightComponents = Object.assign(
-    Object.fromEntries(classes.map((query, index) => ['.bg-' + query, { 'background-color': hexFromArgb(theme.schemes.light[props[index]]) }])),
-    Object.fromEntries(classes.map((query, index) => ['.text-' + query, { 'color': hexFromArgb(theme.schemes.light[props[index]]) }]))
-  )
-  const darkComponents = Object.assign(
-    Object.fromEntries(classes.map((query, index) => ['.bg-' + query, { 'background-color': hexFromArgb(theme.schemes.dark[props[index]]) }])),
-    Object.fromEntries(classes.map((query, index) => ['.text-' + query, { 'color': hexFromArgb(theme.schemes.dark[props[index]]) }]))
-  )
-  const components = {
-    '@media (prefers-color-scheme: dark)': darkComponents,
-    '@media (prefers-color-scheme: light)': lightComponents,
-  }
-  return tailwindPlugin(({ addComponents }) => {
-    addComponents(components)
-  })
-}
+const { mikanUI } = require("mikanui")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -29,6 +7,8 @@ module.exports = {
 		extend: {},
 	},
 	plugins: [
-    materialColorPlugin('#f0e22f'),
+    mikanUI({
+      seedColor: '#f0e22f',
+    }),
   ],
 }
