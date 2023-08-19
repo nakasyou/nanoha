8dd007add00861fdf3ed73681265aa2166570e32
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-19 20:34:23 +0900

    Update ScanDialog.tsx

--- src/app/note/components/ScanDialog.tsx
+++ src/app/note/components/ScanDialog.tsx
@@ -127,7 +127,7 @@ export default () => {
                 }}>カメラを起動する</button>
               </div> }
               <div>
-                <div className='relative w-full h-[80vh]'>
+                <div className='relative w-full h-[70vh]'>
                   <img ref={imageRef} className="absolute w-full h-full object-contain" />
                   <svg viewBox={sheetSvgViewBox} className='absolute touch-none w-full h-full object-contain' ref={svgRef} style={{
                     //bottom: sheetSvgViewBox.replace(/.+ .+ .+ /, "")+  "px"
