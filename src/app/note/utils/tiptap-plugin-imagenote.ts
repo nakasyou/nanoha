a618184ee09e8c6c29796916c1b784f9442d9a76
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-26 10:50:47 +0000

    fix

---
+++ src/app/note/utils/tiptap-plugin-imagenote.ts
@@ -0,0 +1,91 @@
+import {
+  mergeAttributes,
+  Node,
+  nodeInputRule,
+} from '@tiptap/core'
+
+export interface ImegeNoteOptions {
+  HTMLAttributes: Record<string, any>,
+}
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    image: {
+      /**
+       * Add an image
+       */
+      setImage: (options: { src: string, alt?: string, title?: string }) => ReturnType,
+    }
+  }
+}
+
+export const inputRegex = /(?:^|\s)(!\[(.+|:?)]\((\S+)(?:(?:\s+)["'](\S+)["'])?\))$/
+
+export const Image = Node.create<ImegeNoteOptions>({
+  name: 'image',
+
+  addOptions() {
+    return {
+      HTMLAttributes: {},
+    }
+  },
+
+  draggable: true,
+
+  addAttributes() {
+    return {
+      src: {
+        default: null,
+      },
+      alt: {
+        default: null,
+      },
+      title: {
+        default: null,
+      },
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'div',
+        getAttrs (node) {
+          if (typeof node === "string") {
+            return false
+          }
+          return ('nanohaimagenote' in node.dataset as unknown as null)
+        }
+      },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['img', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes)]
+  },
+
+  addCommands() {
+    return {
+      setImage: options => ({ commands }) => {
+        return commands.insertContent({
+          type: this.name,
+          attrs: options,
+        })
+      },
+    }
+  },
+
+  addInputRules() {
+    return [
+      nodeInputRule({
+        find: inputRegex,
+        type: this.type,
+        getAttributes: match => {
+          const [,, alt, src, title] = match
+
+          return { src, alt, title }
+        },
+      }),
+    ]
+  },
+})
