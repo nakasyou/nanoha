7850d66c8f1d17e0c76d09b1b5ddb65932a72fb6
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-27 13:48:34 +0900

    Update ImageNote.tsx

--- src/app/note/components/ImageNote.tsx
+++ src/app/note/components/ImageNote.tsx
@@ -1,4 +1,4 @@
-import { ModeContext } from "../index.tsx"
+import { UserStateContext } from "../index.tsx"
 import { viewClasses, hideClasses } from '../const/sheetClasses.ts'
 
 import {
@@ -8,11 +8,30 @@ import { useEffect, useRef, useContext } from "react"
 import classNames from "classnames"
 
 export interface Props {
-  mode: "edit" | "play"
-  isView: boolean
-  defaultContent: string
-  setEditorState: (editor: Editor | null) => void
+  imageBlob: Blob
+  paths: string[]
 }
-export default (props: Props) => {
-  const modeData = useContext(ModeContext)
+export default const ImageNote = (props: Props) => {
+  const userState = useContext(UserStateContext)
+  const blobUrl = URL.createObjectURL(props.imageBlob)
+
+  const [viewbox, setViewBox] = useState('0 0 0 0')
+  
+  const image = new Image()
+  image.onload = () => {
+    setViewbox(`0 0 ${image.width} ${image.height}`)
+  }
+  image.src = blobUrl
+  return <div>
+    <div className='relative'>
+      <img className='absolute top-0' src={ blobUrl } alt='Scaned Image' />
+      <svg className='absolute top-0' viewbox={ viewbox }>
+        {
+          props.paths.map((path, index) => {
+            return <path d={ path } key={ index } stroke="#f002" strokeWidth="20" fill="none" />
+          })
+        }
+      </svg>
+    </div>
+  </div>
 }
