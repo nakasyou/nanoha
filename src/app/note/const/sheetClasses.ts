8eaafe4d99166d352e1b068a58acb399c940badd
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-30 22:07:59 +0900

    Update sheetClasses.ts

--- src/app/note/const/sheetClasses.ts
+++ src/app/note/const/sheetClasses.ts
@@ -1,2 +1,2 @@
-export const viewClasses = ['bg-red-100', 'stroke-[#f002] dark:stroke-[#f000]']
+export const viewClasses = ['bg-red-100', 'stroke-[#f002]', 'dark:stroke-[#f000]']
 export const hideClasses = ['bg-red-500', 'text-transparent', 'stroke-[#f00]']
