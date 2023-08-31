7551b6f304ecf2d6a13b5eef900b8892cc08238f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-31 09:11:18 +0900

    Update index.tsx

--- src/app/note/index.tsx
+++ src/app/note/index.tsx
@@ -97,7 +97,7 @@ export default function(props: Props){
         isMenuActive && <div class='w-screen h-screen fixed top-0 bottom-0'>
           <div className='flex'>
             <div>Menu</div>
-            <button onClick={setMenu(false)}>
+            <button onClick={setIsMenuActive(false)}>
               <IconX />
             </button>
           </div>
