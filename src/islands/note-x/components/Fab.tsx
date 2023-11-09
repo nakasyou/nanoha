deb98f77242bd2f2c18f0a71791d3c66a34c161b
Shotaro Nakamura <syoutarou1103@outlook.jp>
2023-11-09 12:40:24 +0000

    ダブルタップ拡大め..

--- src/islands/note-x/components/Fab.tsx
+++ src/islands/note-x/components/Fab.tsx
@@ -16,19 +16,19 @@ export default (props: Props) => {
     const [isOpen, setIsOpen] = createSignal(false)
     
     return <>
-      <div class="grid gap-2 justify-center mb-3 transition duration-75 origin-bottom" classList={{
+      <div class="grid gap-2 justify-center mb-3 transition duration-75 origin-bottom touch-manipulation" classList={{
         'scale-0': !isOpen(),
         'scale-100': isOpen()
       }}>
-        <div class="small-fab flex justify-center items-center" onClick={() => props.onAddTextNote && props.onAddTextNote()}>
+        <div class="small-fab flex justify-center items-center touch-manipulation" onClick={() => props.onAddTextNote && props.onAddTextNote()}>
           <div innerHTML={removeIconSize(IconNote)} class="w-5 h-5" />
         </div>
-        <div class="small-fab flex justify-center items-center" onClick={() => props.onAddImageNote && props.onAddImageNote()}>
+        <div class="small-fab flex justify-center items-center touch-manipulation" onClick={() => props.onAddImageNote && props.onAddImageNote()}>
           <div innerHTML={removeIconSize(IconNote)} class="w-5 h-5" />
         </div>
       </div>
       
-      <div class="fab flex justify-center items-center" onClick={() => {
+      <div class="fab flex justify-center items-center touch-manipulation" onClick={() => {
         setIsOpen(!isOpen())
       }}>
         <div innerHTML={removeIconSize(IconPlus)} class="w-8 h-8" />
@@ -39,7 +39,7 @@ export default (props: Props) => {
   return <div class="fixed right-0 bottom-0 m-4">
     {
       noteBookState.isEditMode ? <EditModeFab /> :
-      <button class="fab flex justify-center items-center"
+      <button class="fab flex justify-center items-center touch-manipulation"
         onClick={() => {
           setNoteBookState('isEditMode', true)
         }} innerHTML={Pencil} />
