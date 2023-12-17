03a8620a57ed62b80e3a3f804a2eae0dd0d0792f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-12-17 08:45:09 +0000

    feat: ヘッダーで編集モードを切り替えられるように

--- src/islands/note-x/components/Header.tsx
+++ src/islands/note-x/components/Header.tsx
@@ -1,24 +1,41 @@
-import IconArrowLeft from '@tabler/icons/arrow-left.svg?raw'
-import IconPlayerPlay from '@tabler/icons/player-play.svg?raw'
+import IconArrowLeft from "@tabler/icons/arrow-left.svg?raw"
+import IconPlayerPlay from "@tabler/icons/player-play.svg?raw"
+import IconPencil from "@tabler/icons/pencil.svg?raw"
 
-import { removeIconSize } from '../utils/icon/removeIconSize'
+import { removeIconSize } from "../utils/icon/removeIconSize"
 
-import { noteBookState, setNoteBookState } from '../App'
+import { noteBookState, setNoteBookState } from "../App"
+import { Match, Show, Switch } from "solid-js"
 
 const Header = () => {
-  return <div class="w-full lg:w-10 lg:px-2 py-2 bg-surface-variant z-10 h-auto lg:h-[100dvh]">
-    <div class="flex justify-between items-center flex-row lg:flex-col h-full">
-      <div>
-        <a href="/app">
-          <div innerHTML={removeIconSize(IconArrowLeft)} class='w-8 h-8' />
-        </a>
-      </div>
-      {
-        noteBookState.isEditMode && <div class="flex items-center">
-          <button class="w-8 h-8" innerHTML={removeIconSize(IconPlayerPlay)} onClick={() => setNoteBookState('isEditMode', false)} />
+  return (
+    <div class="w-full lg:w-10 lg:px-2 py-2 bg-surface-variant z-10 h-auto lg:h-[100dvh]">
+      <div class="flex justify-between items-center flex-row lg:flex-col h-full">
+        <div>
+          <a href="/app">
+            <div innerHTML={removeIconSize(IconArrowLeft)} class="w-8 h-8" />
+          </a>
         </div>
-      }
+        <div class="flex items-center">
+          <Show
+            when={noteBookState.isEditMode}
+            fallback={
+              <button
+                class="w-8 h-8"
+                innerHTML={removeIconSize(IconPencil)}
+                onClick={() => setNoteBookState("isEditMode", true)}
+              />
+            }
+          >
+            <button
+              class="w-8 h-8"
+              innerHTML={removeIconSize(IconPlayerPlay)}
+              onClick={() => setNoteBookState("isEditMode", false)}
+            />
+          </Show>
+        </div>
+      </div>
     </div>
-  </div>
+  )
 }
 export default Header
