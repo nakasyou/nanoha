3cdba5c25b5964750fa519b57434e0e642d6dd06
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-02-06 12:17:38 +0000

    create note

--- src/islands/note-list/list.tsx
+++ src/islands/note-list/list.tsx
@@ -8,20 +8,41 @@ const NoteListLoader = lazy(async () => {
   const allNotes = await db.notes.toArray()
 
   return {
-    default: () => <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+    default: (props: { update (): void }) => <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
       <For each={allNotes}>{(note) => {
-        return <a href={`/app/notes/local-${note.id}`}><div class="border p-2 bg-surface rounded border-outline">
-          <div class="text-lg">{ note.name }</div>
+        return <div class="border p-2 bg-surface rounded border-outline">
+          <a href={`/app/notes/local-${note.id}`}><div class="text-lg">{ note.name }</div></a>
           <div>
-            <div>Last Updated: {note.updated.toString()}</div>
+            <div>Last Updated: {new Intl.DateTimeFormat('ja-jp', {
+              hour: 'numeric',
+              minute: 'numeric',
+              second: 'numeric',
+              day: 'numeric',
+              month: 'numeric',
+              year: 'numeric'
+            }).format(note.updated)}</div>
           </div>
-        </div></a>
+          <div class="text-right">
+            <button
+              class="underline hover:no-underline"
+              onClick={async (e) => {
+                e.preventDefault()
+                if (!confirm(`ノート「${note.name}」を削除しますか？`)) {
+                  return
+                }
+                await db.notes.delete(note.id!)
+                props.update()
+              }}>削除</button>
+          </div>
+        </div>
       }}</For>
     </div>
   }
 })
 export const NoteList = () => {
   return <Suspense fallback={<div>Loading...</div>}>
-    <NoteListLoader />
+    <NoteListLoader update={() => {
+      location.reload()
+    }}/>
   </Suspense>
 }
