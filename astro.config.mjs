b6b54e6c6f5f2e1b5697533e3f7e410cb2d64079
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-02-07 11:00:11 +0000

    feat(seo): add sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
 import deno from "./integrations/deno";
-
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import cloudflare from "@astrojs/cloudflare";
-import { passthroughImageService } from 'astro/config'
+import { passthroughImageService } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  site: "https://nanoha.pages.dev",
   adapter: process.env.IS_CF ? cloudflare() : deno(),
-  integrations: [tailwind({}), solidJs(), svelte()],
+  integrations: [tailwind({}), solidJs(), svelte(), sitemap()],
   image: {
     service: passthroughImageService()
   }
