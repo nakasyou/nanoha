5b9aa2ae921a4a3571d0bafd5487ca06ba1af7d0
Nakamura Shotaro <syoutarou1103@outlook.jp>
2024-03-31 18:46:09 +0900

    wip: qwik

---
+++ src/islands/note-list/list.qwik.tsx
@@ -0,0 +1,62 @@
+/** @jsxImportSource @builder.io/qwik */
+import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik'
+import { NotesDB, type Notes } from '../note/notes-schema'
+
+export const NoteList = component$(() => {
+  const notes = useSignal<Notes[] | null>(null)
+  let db = null//useSignal<NotesDB | null>(null)
+
+  useVisibleTask$(() => {
+    const notesDB = new NotesDB()
+    db = notesDB
+
+    ;(async () => {
+      notes.value = await notesDB.notes.toArray()
+    })()
+  })
+  return (
+    <div>
+      {notes.value ? (
+        <>
+          {notes.value.map((note) => (
+            <div class="border p-2 bg-surface rounded border-outline">
+              <a href={`/app/notes/local-${note.id}`}>
+                <div class="text-lg">{note.name}</div>
+              </a>
+              <div>
+                <div>
+                  Last Updated:{' '}
+                  {new Intl.DateTimeFormat('ja-jp', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                    day: 'numeric',
+                    month: 'numeric',
+                    year: 'numeric'
+                  }).format(note.updated)}
+                </div>
+              </div>
+              <div class="text-right">
+                <button
+                  class="underline hover:no-underline"
+                  onClick$={async (e) => {
+                    /*e.preventDefault()
+                    if (!confirm(`ノート「${note.name}」を削除しますか？`)) {
+                      return
+                    }
+                    await db.notes.delete(note.id!)
+                    props.update()*/
+                  }}
+                >
+                  削除
+                </button>
+              </div>
+            </div>
+          ))}
+        </>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  )
+})
