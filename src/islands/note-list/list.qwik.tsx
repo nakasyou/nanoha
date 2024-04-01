071fda38c48f58ae978d49f8aa3c817e0c3a7d4a
Nakamura Shotaro <syoutarou1103@outlook.jp>
2024-04-01 18:46:32 +0900

    feat: qwik化OK?

--- src/islands/note-list/list.qwik.tsx
+++ src/islands/note-list/list.qwik.tsx
@@ -4,12 +4,11 @@ import { NotesDB, type Notes } from '../note/notes-schema'
 
 export const NoteList = component$(() => {
   const notes = useSignal<Notes[] | null>(null)
-  let db = null//useSignal<NotesDB | null>(null)
+  let db = null //useSignal<NotesDB | null>(null)
 
   useVisibleTask$(() => {
     const notesDB = new NotesDB()
     db = notesDB
-
     ;(async () => {
       notes.value = await notesDB.notes.toArray()
     })()
@@ -17,45 +16,49 @@ export const NoteList = component$(() => {
   return (
     <div>
       {notes.value ? (
-        <>
-          {notes.value.map((note) => (
-            <div class="border p-2 bg-surface rounded border-outline">
-              <a href={`/app/notes/local-${note.id}`}>
-                <div class="text-lg">{note.name}</div>
-              </a>
-              <div>
+        notes.value.length === 0 ? (
+          <div>ノートがありません</div>
+        ) : (
+          <div>
+            {notes.value.map((note) => (
+              <div class="border p-2 bg-surface rounded border-outline">
+                <a href={`/app/notes/local-${note.id}`}>
+                  <div class="text-lg">{note.name}</div>
+                </a>
                 <div>
-                  Last Updated:{' '}
-                  {new Intl.DateTimeFormat('ja-jp', {
-                    hour: 'numeric',
-                    minute: 'numeric',
-                    second: 'numeric',
-                    day: 'numeric',
-                    month: 'numeric',
-                    year: 'numeric'
-                  }).format(note.updated)}
+                  <div>
+                    Last Updated:{' '}
+                    {new Intl.DateTimeFormat('ja-jp', {
+                      hour: 'numeric',
+                      minute: 'numeric',
+                      second: 'numeric',
+                      day: 'numeric',
+                      month: 'numeric',
+                      year: 'numeric'
+                    }).format(note.updated)}
+                  </div>
                 </div>
-              </div>
-              <div class="text-right">
-                <button
-                  class="underline hover:no-underline"
-                  onClick$={async (e) => {
-                    /*e.preventDefault()
+                <div class="text-right">
+                  <button
+                    class="underline hover:no-underline"
+                    onClick$={async (e) => {
+                      /*e.preventDefault()
                     if (!confirm(`ノート「${note.name}」を削除しますか？`)) {
                       return
                     }
                     await db.notes.delete(note.id!)
                     props.update()*/
-                  }}
-                >
-                  削除
-                </button>
+                    }}
+                  >
+                    削除
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-        </>
+            ))}
+          </div>
+        )
       ) : (
-        <div></div>
+        <div>読み込み中...</div>
       )}
     </div>
   )
