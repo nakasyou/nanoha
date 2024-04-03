967dd89ed8ae96c81b40ed31b5db525dbd226e26
Nakamura Shotaro <syoutarou1103@outlook.jp>
2024-04-03 19:01:56 +0900

    perf: TextNoteのpreviewモード

--- src/islands/note/components/notes/TextNote/TextNote.tsx
+++ src/islands/note/components/notes/TextNote/TextNote.tsx
@@ -1,10 +1,10 @@
-
 import type { NoteComponent, NoteComponentProps } from '../../notes-utils'
 import StarterKit from '@tiptap/starter-kit'
 import type { TextNoteData } from './types'
 import { ExtensionSheet } from './tiptap/PluginSheet'
 import { Underline } from '@tiptap/extension-underline'
 import {
+  For,
   Match,
   Show,
   Switch,
@@ -18,7 +18,7 @@ import IconNote from '@tabler/icons/outline/note.svg?raw'
 import IconBold from '@tabler/icons/outline/bold.svg?raw'
 import IconUnderline from '@tabler/icons/outline/underline.svg?raw'
 
-import { Editor } from '@tiptap/core'
+import { Editor, isActive } from '@tiptap/core'
 import { Dialog } from '../../utils/Dialog'
 import { Controller } from '../../note-components/Controller'
 import { noteBookState } from '../../../store'
@@ -38,12 +38,38 @@ export const TextNote = ((props: Props) => {
 
   const [getEditor, setEditor] = createSignal<Editor>()
 
-  const [getIsFocused, setIsFocused] = createSignal(false)
   const [getIsActiveSheet, setIsActiveSheet] = createSignal(false)
   const [getIsActiveBold, setIsActiveBold] = createSignal(false)
   const [getIsActiveUndlerline, setIsActiveUndlerline] = createSignal(false)
+  const [getIsActive, setIsActive] = createSignal(false)
+  const [isShowCloseDialog, setIsShowCloseDialog] = createSignal(false)
+
+  const controllerItems = [
+    {
+      icon: IconNote,
+      toggle: 'toggleSheet',
+      isActive: getIsActiveSheet
+    },
+    {
+      icon: IconBold,
+      toggle: 'toggleBold',
+      isActive: getIsActiveBold
+    },
+    {
+      icon: IconUnderline,
+      toggle: 'toggleUnderline',
+      isActive: getIsActiveUndlerline
+    }
+  ] satisfies {
+    icon: string
+    toggle: keyof Editor['commands'] & `toggle${string}`
+    isActive: () => boolean
+  }[]
 
   const startEditor = () => {
+    if (!editorRef) {
+      return
+    }
     const editor = new Editor({
       element: editorRef,
       extensions: [
@@ -63,123 +89,119 @@ export const TextNote = ((props: Props) => {
       setIsActiveUndlerline(editor.isActive('underline'))
     })
   }
+
   onMount(() => {
-    startEditor()
-    /*editorRef.onclick = () => {
-      setIsFocused(true)
-    }*/
     editorRef.onfocus = () => {
       props.focus()
     }
   })
-
-  const [isActive, setIsActive] = createSignal(false)
-
-  createEffect(() => {
-    if (!isActive()) {
-      getEditor()?.destroy()
-    } else {
-      startEditor()
-    }
-  })
-  props.on('focus', (evt) => {
-    setIsActive(evt.isActive)
-  })
-
-
-  const [isShowCloseDialog, setIsShowCloseDialog] = createSignal(false)
   const saveContent = () => {
     props.setNoteData('canToJsonData', 'html', getEditor()?.getHTML() || '')
     props.updated()
   }
+
+  props.on('focus', (evt) => {
+    setIsActive(evt.isActive)
+  })
+
+  createEffect(() => {
+    const isActive = getIsActive()
+    const editor = getEditor()
+    if (isActive) {
+      if (editor?.isDestroyed || !editor) {
+        startEditor()
+      }
+    } else {
+      editor?.destroy()
+    }
+  })
   return (
-    <div>
-      <Show when={!noteBookState.isEditMode}>
-        <div>
-          <Player html={getEditor()?.getHTML() || ''} />
-        </div>
+    <div class="my-2">
+      <Show when={isShowCloseDialog()}>
+        <Dialog
+          onClose={(result) => {
+            if (result) {
+              // 消していいいらしい
+              props.removeNote()
+            }
+            setIsShowCloseDialog(false)
+          }}
+          type="confirm"
+          title="削除しますか?"
+        >
+          ノートを削除すると、元に戻せなくなる可能性があります。
+        </Dialog>
       </Show>
+
+      <Show when={!noteBookState.isEditMode}>
+        <Player html={getEditor()?.getHTML() || ''} />
+      </Show>
+
       <div
         classList={{
-          hidden: !noteBookState.isEditMode
+          hidden: !getIsActive() || !noteBookState.isEditMode
         }}
       >
-        {isShowCloseDialog() && (
-          <Dialog
-            onClose={(result) => {
-              if (result) {
-                // 消していいいらしい
-                props.removeNote()
-              }
-              setIsShowCloseDialog(false)
-            }}
-            type="confirm"
-            title="削除しますか?"
-          >
-            ノートを削除すると、元に戻せなくなる可能性があります。
-          </Dialog>
-        )}
-        <div>
+        <div class="textnote-tiptap-container rounded my-2 border boader-outlined nanohanote-textnote-styler">
           <div
             id="editor"
             ref={editorRef}
             onFocusOut={saveContent}
             onInput={saveContent}
-            contentEditable={true}
-            class="textnote-tiptap-container rounded my-2 border boader-outlined nanohanote-textnote-styler"
           />
         </div>
-        <Show when={isActive()}>
-          <div class="flex flex-col justify-center gap-2">
-            <div class="flex justify-center gap-2 fixed left-0 w-full" style={{
-              top: (getVisualViewport()?.data?.height ?? 0) + (getVisualViewport()?.data?.pageTop ?? 0) - 32 + 'px'
-            }}>
-              {
-                ([
-                  {
-                    icon: IconNote,
-                    toggle: 'toggleSheet',
-                    isActive: getIsActiveSheet
-                  },
-                  {
-                    icon: IconBold,
-                    toggle: 'toggleBold',
-                    isActive: getIsActiveBold
-                  },
-                  {
-                    icon: IconUnderline,
-                    toggle: 'toggleUnderline',
-                    isActive: getIsActiveUndlerline
-                  }
-                ] satisfies ({
-                  icon: string
-                  toggle: keyof Editor['commands']
-                  isActive: () => boolean
-                }[])).map((data) => {
-                  return <button
-                    class="grid drop-shadow-none"
-                    onClick={() => {
-                      getEditor()?.chain().focus()[data.toggle]().run()
-                    }}
-                    classList={{
-                      'bg-secondary-container text-secondary rounded': data.isActive()
-                    }}
-                  >
-                    <div innerHTML={removeIconSize(data.icon)} class="w-8 h-8" />
-                  </button>
-                })
-              }
-            </div>
-            <Controller
-              noteIndex={props.index}
-              notesLength={props.notes.length}
-              onRemove={() => setIsShowCloseDialog(true)}
-              onUpNote={() => props.up()}
-              onDownNote={() => props.down()}
-            />
-          </div>
-        </Show>
       </div>
+      <div
+        classList={{
+          hidden: getIsActive() || !noteBookState.isEditMode
+        }}
+      >
+        <div
+          innerHTML={props.noteData.canToJsonData.html}
+          class="rounded p-2 border boader-outlined"
+          onClick={() => {
+            props.focus()
+          }}
+        />
+      </div>
+
+      <Show when={getIsActive()}>
+        <div class="flex flex-col justify-center gap-2">
+          <div
+            class="flex justify-center gap-2 fixed left-0 w-full"
+            style={{
+              top:
+                (getVisualViewport()?.data?.height ?? 0) +
+                (getVisualViewport()?.data?.pageTop ?? 0) -
+                32 + 'px'
+            }}
+          >
+            <For each={controllerItems}>
+              {(data) => (
+                <button
+                  class="grid drop-shadow-none"
+                  onClick={() => {
+                    getEditor()?.chain().focus()[data.toggle]()
+                  }}
+                  classList={{
+                    'bg-secondary-container text-secondary rounded':
+                      data.isActive()
+                  }}
+                >
+                  <div innerHTML={removeIconSize(data.icon)} class="w-8 h-8" />
+                </button>
+              )}
+            </For>
+          </div>
+          <Controller
+            noteIndex={props.index}
+            notesLength={props.notes.length}
+            onRemove={() => setIsShowCloseDialog(true)}
+            onUpNote={() => props.up()}
+            onDownNote={() => props.down()}
+          />
+        </div>
+      </Show>
     </div>
   )
 }) satisfies NoteComponent
