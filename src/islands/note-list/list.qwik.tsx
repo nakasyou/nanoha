55d2941b98c5412d8a0e9eba3af4236d05cbf5ab
Nakamura Shotaro <syoutarou1103@outlook.jp>
2024-04-23 19:19:39 +0900

    wip

--- src/islands/note-list/list.qwik.tsx
+++ src/islands/note-list/list.qwik.tsx
@@ -5,14 +5,14 @@ import {
   type QRL,
   useOnDocument,
   useSignal,
-  useVisibleTask$,
-} from "@builder.io/qwik";
-import { type Notes, NotesDB } from "../note/notes-schema";
-import classnames from "classnames";
+  useVisibleTask$
+} from '@builder.io/qwik'
+import { type Notes, NotesDB } from '../note/notes-schema'
+import classnames from 'classnames'
 
 const NoteListItem = component$<{
-  note: Notes;
-  update: QRL<() => Promise<void>>;
+  note: Notes
+  update: QRL<() => Promise<void>>
 }>((props) => {
   return (
     <div class="border bg-surface rounded border-outline p-2">
@@ -21,13 +21,14 @@ const NoteListItem = component$<{
       </a>
       <div>
         <div class="text-sm">
-          最終更新: {new Intl.DateTimeFormat("ja-jp", {
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric",
-            day: "numeric",
-            month: "numeric",
-            year: "numeric",
+          最終更新:{' '}
+          {new Intl.DateTimeFormat('ja-jp', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric'
           }).format(props.note.updated)}
         </div>
       </div>
@@ -35,120 +36,124 @@ const NoteListItem = component$<{
         <button
           class="underline hover:no-underline"
           onClick$={async (e) => {
-            e.preventDefault();
+            e.preventDefault()
             if (!confirm(`ノート「${props.note.name}」を削除しますか？`)) {
-              return;
+              return
             }
-            await new NotesDB().notes.delete(props.note.id!);
-            props.update();
+            await new NotesDB().notes.delete(props.note.id!)
+            props.update()
           }}
         >
           削除
         </button>
       </div>
     </div>
-  );
-});
+  )
+})
 
 export const NoteList = component$((props) => {
-  const notes = useSignal<Notes[] | null>(null);
+  const notes = useSignal<Notes[] | null>(null)
 
-  const sortMode = useSignal<"updated" | "name">("updated");
-  const isShownSortModeDialog = useSignal(false);
-  const isStartedSortModeDialog = useSignal(false);
+  const sortMode = useSignal<'updated' | 'name'>('updated')
+  const isShownSortModeDialog = useSignal(false)
+  const isStartedSortModeDialog = useSignal(false)
 
   const update = $(async () => {
-    notes.value = null;
-    const notesDB = new NotesDB();
-    notesDB;
-    notes.value = await notesDB.notes.toArray();
-  });
+    notes.value = null
+    const notesDB = new NotesDB()
+    notesDB
+    notes.value = await notesDB.notes.toArray()
+  })
 
   const loaded = $(() => {
-    update();
-  });
-  useOnDocument("load", loaded);
-  useOnDocument("astro:page-load", loaded);
+    update()
+  })
+  useOnDocument('load', loaded)
+  useOnDocument('astro:page-load', loaded)
   return (
     <div>
       <div class="py-2">
         <button
           onClick$={() => {
-            isShownSortModeDialog.value = true;
+            isShownSortModeDialog.value = true
             setTimeout(() => {
-              isStartedSortModeDialog.value = true;
-            }, 50);
+              isStartedSortModeDialog.value = true
+            }, 50)
           }}
           class="text-on-surface-variant"
         >
-          {{
-            updated: "最終更新日",
-            name: "名前",
-          }[sortMode.value]}
+          {
+            {
+              updated: '最終更新日',
+              name: '名前'
+            }[sortMode.value]
+          }
         </button>
       </div>
       <div class="px-1">
-        {notes.value
-          ? (
-            notes.value.length === 0
-              ? <div>ノートがありません</div>
-              : (
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
-                  {notes.value.map((note) => (
-                    <NoteListItem note={note} update={update} />
-                  ))}
-                </div>
-              )
+        {notes.value ? (
+          notes.value.length === 0 ? (
+            <div>ノートがありません</div>
+          ) : (
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+              {notes.value.map((note) => (
+                <NoteListItem note={note} update={update} />
+              ))}
+            </div>
           )
-          : <div>読み込み中...</div>}
+        ) : (
+          <div>読み込み中...</div>
+        )}
       </div>
 
       <div
         class={classnames([
-          "fixed top-0 left-0 w-full h-[100dvh] z-20 transition-colors",
+          'fixed top-0 left-0 w-full h-[100dvh] z-20 transition-colors flex items-end',
           {
-            "hidden": !isShownSortModeDialog.value,
+            hidden: !isShownSortModeDialog.value
           },
-          isStartedSortModeDialog.value ? "bg-[#000a]" : "bg-transparent",
+          isStartedSortModeDialog.value ? 'bg-[#000a]' : 'bg-transparent'
         ])}
         onClick$={() => {
-          isStartedSortModeDialog.value = false;
+          isStartedSortModeDialog.value = false
           setTimeout(() => {
-            isShownSortModeDialog.value = false;
-          }, 200);
+            isShownSortModeDialog.value = false
+          }, 200)
         }}
       >
         <div
           class={classnames([
-            "transition-transform duration-150 w-1/2 h-full rounded-l-3xl bg-background p-4 flex flex-col",
+            'transition-transform duration-150 w-full md:w-1/2 md:h-full rounded-l-3xl bg-background p-4 flex flex-col',
             isStartedSortModeDialog.value
-              ? "translate-x-full"
-              : "translate-x-[200%]",
+              ? 'translate-y-0 md:translate-x-full' // Open
+              : '-translate-y-full md:translate-x-[200%]' // Close
           ])}
           onClick$={(evt) => {
-            evt.stopPropagation();
+            evt.stopPropagation()
           }}
         >
           <div class="text-2xl text-center">Sorting</div>
           <div class="grow flex items-center">
             <div class="grid grid-rows-2 rounded-3xl w-full">
               <button
-                onClick$={() => sortMode.value = "updated"}
-                class={classnames("transition-colors p-2 rounded-t-3xl", {
-                  "bg-secondary text-on-secondary":
-                    sortMode.value === "updated",
-                  "bg-surface text-on-surface border border-outlined":
-                    sortMode.value !== "updated",
-                })}
+                onClick$={() => (sortMode.value = 'updated')}
+                class={classnames(
+                  'p-2 rounded-t-3xl',
+                  sortMode.value === 'updated'
+                    ? 'bg-secondary text-on-secondary'
+                    : 'bg-surface text-on-surface border border-outlined'
+                )}
               >
                 最終更新
               </button>
               <button
-                onClick$={() => sortMode.value = "name"}
-                class={classnames("transition-colors p-2 rounded-b-3xl", {
-                  "bg-secondary text-on-secondary": sortMode.value === "name",
-                  "bg-surface text-on-surface border border-outlined": sortMode.value !== "name",
-                })}
+                onClick$={() => (sortMode.value = 'name')}
+                class={classnames(
+                  'p-2 rounded-b-3xl',
+                  sortMode.value === 'name'
+                    ? 'bg-secondary text-on-secondary'
+                    : 'bg-surface text-on-surface border border-outlined'
+                )}
               >
                 名前
               </button>
@@ -157,5 +162,5 @@ export const NoteList = component$((props) => {
         </div>
       </div>
     </div>
-  );
-});
+  )
+})
