400c3d48a96ddb3c4e0421f162b73e845fbbc2b3
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2024-07-15 17:06:25 +0900

    feat: ドラッグ&ドロップによるノートの並び替え (#185)
    * feat: 直感的なノートソート
    
    * feat: 中心を基準としたソート
    
    * feat: ソート中にスクロール可能に

--- src/islands/note/components/Notes.tsx
+++ src/islands/note/components/Notes.tsx
@@ -5,18 +5,23 @@ import {
   type JSX,
   createEffect,
   Show,
+  onMount,
 } from 'solid-js'
 import { Key } from '@solid-primitives/keyed'
 import { moveArray } from '../utils/array/moveArray'
 
 import type { Note } from './notes-utils'
-import { notes, setNoteBookState } from '../store'
+import { noteBookState, notes, setNoteBookState } from '../store'
 import { Controller } from './note-components/Controller'
 import { Dialog } from './utils/Dialog'
+import iconGripHorizontal from '@tabler/icons/outline/grip-horizontal.svg?raw'
+import iconX from '@tabler/icons/outline/x.svg?raw'
+import { removeIconSize } from '../utils/icon/removeIconSize'
 
 export default (props: {
   notes: Note[]
   setNotes: Setter<Note[]>
+  scrollParent: HTMLDivElement
 }) => {
   const [getFocusedIndex, setFocusedIndex] = createSignal(0)
   const [getIndexToRemove, setIndexToRemove] = createSignal<null | number>(null)
@@ -28,6 +33,17 @@ export default (props: {
     notes.notes
     handleUpdate()
   })
+
+  const getNoteRect: Record<string, () => DOMRect> = {}
+
+  const [getDraggedNoteIndex, setDraggedNoteIndex] = createSignal<
+    number | null
+  >(null)
+  const [getDraggedNoteYPosition, setDraggedNoteYPosition] = createSignal<
+    number | null
+  >(null)
+  const [getDragTarget, setDragTarget] = createSignal<number | null>(null)
+  const [getIsDragUp, setIsDragUp] = createSignal(false)
   return (
     <div class="flex flex-col gap-2 my-2">
       <Show when={getIndexToRemove() !== null}>
@@ -60,58 +76,165 @@ export default (props: {
             nowNote.noteData
             handleUpdate()
           })
-          return (
-            <div>
-              <div class="border p-1">
-                <NoteComponent
-                  noteData={note().noteData}
-                  setNoteData={note().setNoteData}
-                  focus={() => {
-                    for (const eachNote of props.notes) {
-                      const isActive =
-                        eachNote.noteData.id === note().noteData.id
-                      for (const focusEventListener of eachNote.events.focus ||
-                        []) {
-                        focusEventListener({
-                          isActive,
-                        })
-                      }
-                      if (isActive) {
-                        setFocusedIndex(index)
-                      }
+          const noteElement = (
+            <div class="border p-1 grow">
+              <NoteComponent
+                noteData={note().noteData}
+                setNoteData={note().setNoteData}
+                focus={() => {
+                  for (const eachNote of props.notes) {
+                    const isActive = eachNote.noteData.id === note().noteData.id
+                    for (const focusEventListener of eachNote.events.focus ||
+                      []) {
+                      focusEventListener({
+                        isActive,
+                      })
                     }
-                  }}
-                  updated={handleUpdate}
-                  on={(type, listener) => {
-                    const thisNote = props.notes[index()]
-                    if (!thisNote) return
-                    if (!thisNote.events[type]) {
-                      thisNote.events[type] = []
+                    if (isActive) {
+                      setFocusedIndex(index)
                     }
-                    thisNote.events[type]?.push(listener)
-                  }}
-                  index={index()}
-                  notes={props.notes}
-                />
-              </div>
-              <Show when={getFocusedIndex() === index()}>
-                <Controller
-                  onRemove={() => {
-                    setIndexToRemove(index())
-                  }}
-                  onDownNote={() => {
-                    setFocusedIndex(index() + 1)
-                    props.setNotes(moveArray(props.notes, index(), index() + 1))
-                  }}
-                  onUpNote={() => {
-                    setFocusedIndex(index() - 1)
-                    props.setNotes(moveArray(props.notes, index(), index() - 1))
-                  }}
-                  noteIndex={index()}
-                  notesLength={props.notes.length}
-                />
-              </Show>
+                  }
+                }}
+                updated={handleUpdate}
+                on={(type, listener) => {
+                  const thisNote = props.notes[index()]
+                  if (!thisNote) return
+                  if (!thisNote.events[type]) {
+                    thisNote.events[type] = []
+                  }
+                  thisNote.events[type]?.push(listener)
+                }}
+                index={index()}
+                notes={props.notes}
+              />
             </div>
+          )
+          let notePosition!: HTMLDivElement
+          let noteElem!: HTMLDivElement
+
+          let downedPointerId: number | null = null
+          const getRectCenterY = (rect: DOMRect) => rect.top + rect.height / 2
+          const calcTarget = () => {
+            const thisNoteRect = notePosition.getBoundingClientRect()
+            const draggingRect = noteElem.getBoundingClientRect()
+
+            const isUP = thisNoteRect.top > draggingRect.top
+            setIsDragUp(isUP)
+
+            const noteRects: DOMRect[] = []
+            for (const {
+              noteData: { id },
+            } of props.notes) {
+              noteRects.push(getNoteRect[id]!())
+            }
+            if (isUP) {
+              for (let i = 0; i < index(); i++) {
+                const rect = noteRects[i]!
+                if (getRectCenterY(draggingRect) < getRectCenterY(rect)) {
+                  setDragTarget(i)
+                  return
+                }
+              }
+            } else {
+              for (let i = noteRects.length - 1; i > index(); i--) {
+                const rect = noteRects[i]!
+                if (getRectCenterY(draggingRect) > getRectCenterY(rect)) {
+                  return setDragTarget(i)
+                }
+              }
+            }
+            setDragTarget(index())
+          }
+          onMount(() => {
+            getNoteRect[note().noteData.id] = () =>
+              noteElem.getBoundingClientRect()
+          })
+          const [getDraggable] = createSignal<JSX.Element | null>(
+            <div class="h-24 bg-blue-300" />,
+          )
+
+          let scrollIntervalId: number | null = null
+          let lastPointerEvent: PointerEvent | null = null
+          return (
+            <>
+              <Show when={index() === getDragTarget() && getIsDragUp()}>
+                {getDraggable()}
+              </Show>
+              <div ref={notePosition} />
+              <div
+                class="flex flex-col"
+                ref={noteElem}
+                classList={{
+                  fixed: getDraggedNoteIndex() === index(),
+                }}
+                style={{
+                  ...(getDraggedNoteIndex() === index()
+                    ? {
+                        top: `${getDraggedNoteYPosition()}px`,
+                      }
+                    : {}),
+                }}
+              >
+                <Show when={noteBookState.isEditMode && getFocusedIndex() === index()}>
+                  <div class="flex gap-1">
+                    <button
+                      type="button"
+                      onPointerDown={(e) => {
+                        if (downedPointerId) {
+                          return
+                        }
+                        downedPointerId = e.pointerId
+                        scrollIntervalId = window.setInterval(() => {
+                          if (!lastPointerEvent) {
+                            return
+                          }
+                          const vy = lastPointerEvent.clientY < 50 ? -1 : window.innerHeight - lastPointerEvent.clientY < 50 ? 1 : 0
+                          if (vy === 0) {
+                            return
+                          }
+                          props.scrollParent.scrollBy({
+                            top: 100 * vy,
+                            //behavior: 'smooth'
+                          })
+                        }, 100)
+                        setDragTarget(index())
+                        setDraggedNoteYPosition(e.clientY)
+                        setDraggedNoteIndex(index())
+                        calcTarget()
+                      }}
+                      onPointerMove={(e) => {
+                        if (downedPointerId === e.pointerId) {
+                          lastPointerEvent = e
+                          const target = e.currentTarget as HTMLButtonElement
+                          target.setPointerCapture(e.pointerId)
+                          setDraggedNoteYPosition(e.clientY)
+                          calcTarget()
+                        }
+                      }}
+                      onPointerUp={() => {
+                        clearInterval(scrollIntervalId ?? void 0)
+                        props.setNotes(
+                          moveArray(props.notes, index(), getDragTarget() ?? 0),
+                        )
+                        downedPointerId = null
+                        setDraggedNoteIndex(null)
+                        setDraggedNoteYPosition(null)
+                        setDragTarget(null)
+                      }}
+                      class="touch-none w-8 h-8 text-gray-400"
+                      innerHTML={removeIconSize(iconGripHorizontal)}
+                    />
+                    <button type="button" onClick={() => {
+                      setIndexToRemove(index())
+                    }} class="touch-none w-8 h-8" innerHTML={removeIconSize(iconX)} />
+                  </div>
+                </Show>
+                {noteElement}
+              </div>
+              <Show when={index() === getDragTarget() && !getIsDragUp()}>
+                {getDraggable()}
+              </Show>
+            </>
           )
         }}
       </Key>
