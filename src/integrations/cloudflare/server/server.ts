a2d96d9a37804d80203eefc62d0d851c78492d75
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-11-30 06:23:19 +0000

    pwd

--- src/integrations/cloudflare/server/server.ts
+++ src/integrations/cloudflare/server/server.ts
@@ -1,11 +1,14 @@
 import { App } from 'astro/app'
 // @ts-check
 import { Hono } from 'hono'
+import { basicAuth } from 'hono/basic-auth'
 
 export function createExports(manifest: import('astro').SSRManifest) {
   const app = new App(manifest)
 
   const honoApp = new Hono()
+
+  honoApp.use(basicAuth({verifyUser: (username, password, c) => true ,}))
 
   honoApp.all('/*', async (c) => {
     const res = await app.render(c.req.raw)
